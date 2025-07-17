af3f858f59427cd3d07267830d366a33ecf2a01a
Tri dep trai <bileyghas1eye@gmail.com>
2025-07-18 04:32:19 +0700

    update bt bai 3

--- C1124L1/module3/b2_swap_erd.sql
+++ C1124L1/module3/b2_swap_erd.sql
@@ -1,33 +1,34 @@
-create database if not exists ordersmanagement;
+drop database if exists ordersmanagement;
+create database ordersmanagement;
 use ordersmanagement;
 
 create table NhaCC(
-	id int primary key auto_increment unique,
+	id_ncc int primary key auto_increment unique,
     name varchar(50),
     location varchar(50)
 );
 
 create table PhoneNumber(
-	id int primary key auto_increment,
+	id_phone int primary key auto_increment,
     id_nhacc int,
     phone_number varchar(20),
-    foreign key(id_nhacc) references NhaCC(id)
+    foreign key(id_nhacc) references NhaCC(id_ncc)
 );
 
 create table DonDH(
-	id int primary key auto_increment unique,
+	id_don_dh int primary key auto_increment unique,
     ngay_DH date,
     id_nha_cc int,
-    foreign key(id_nha_cc) references NhaCC(id)
+    foreign key(id_nha_cc) references NhaCC(id_ncc)
 );
 
 create table PhieuNhap(
-	id int primary key auto_increment unique,
+	id_pn int primary key auto_increment unique,
     ngay_nhap date
 );
 
 create table VatTu(
-	id int primary key auto_increment unique,
+	id_vt int primary key auto_increment unique,
     name varchar(50)
 );
 
@@ -37,12 +38,12 @@ create table DonNhap(
     don_gia float,
     so_luong int,
     constraint id primary key(id_vat_tu,id_phieu_nhap),
-    foreign key(id_vat_tu) references VatTu(id),
-    foreign key(id_phieu_nhap) references PhieuNhap(id)
+    foreign key(id_vat_tu) references VatTu(id_vt),
+    foreign key(id_phieu_nhap) references PhieuNhap(id_pn)
 );
 
 create table PhieuXuat(
-	id int primary key auto_increment unique,
+	id_px int primary key auto_increment unique,
     ngay_xuat date
 );
 
@@ -52,14 +53,14 @@ create table DonXuat(
     don_gia float,
     so_luong int,
     constraint id primary key(id_vat_tu,id_phieu_xuat),
-    foreign key(id_vat_tu) references VatTu(id),
-    foreign key(id_phieu_xuat) references PhieuXuat(id)
+    foreign key(id_vat_tu) references VatTu(id_vt),
+    foreign key(id_phieu_xuat) references PhieuXuat(id_px)
 );
 
 create table ChiTietDonDH(
 	id_don_hang int,
     id_vat_tu int,
     constraint id primary key(id_don_hang,id_vat_tu),
-    foreign key(id_don_hang) references DonDH(id),
-    foreign key(id_vat_tu) references VatTu(id)
+    foreign key(id_don_hang) references DonDH(id_don_dh),
+    foreign key(id_vat_tu) references VatTu(id_vt)
 );
